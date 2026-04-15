fcdecfbaccdbcab549700f4be0b6e982f96700b1
smaldos64 <gudumholm34@gmail.com>
2026-04-15 08:54:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VS2022_GitHub_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
